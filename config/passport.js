30b9c00eacdb5a813adcb093b423af3e3fa6d8d9
houssem <houssemm09@gmail.com>
2021-05-15 05:01:35 +0100

    passport added

--- config/passport.js
+++ config/passport.js
@@ -1,2 +1,15 @@
 const passport = require('passport');
 const BearerStrategy = require('passport-http-bearer').Strategy;
+var User = require('../models/user');
+const jwt = require('jsonwebtoken');
+
+passport.use(
+    new BearerStrategy(async (token, done) => {
+        const tokenData = await jwt.verify(token, 'RANDOM_TOKEN');
+        const user = await User.findOne({ _id: tokenData.data._id});
+        if(!user) {
+            return done(null, false);
+        } 
+        else return done(null, {user});
+    })
+);
